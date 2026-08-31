acadc37bdbf904b8a42df2e546fdbb22b7e3c971
Forestwolf-ai <jerrymouse2024@aka.yeah.net>
2026-08-31 13:32:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from app import create_app, db
-from app.scheduler import init_scheduler
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+    from app.scheduler import init_scheduler
     init_scheduler(app)
-    app.run(host=app.config['SERVER_HOST'], port=app.config['SERVER_PORT'], debug=app.config['SERVER_DEBUG'])
+    app.run(
+        host=app.config.get('SERVER_HOST', '0.0.0.0'),
+        port=app.config.get('SERVER_PORT', 5000),
+        debug=app.config.get('SERVER_DEBUG', False)
+    )
